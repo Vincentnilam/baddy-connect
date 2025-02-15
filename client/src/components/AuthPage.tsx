6a80e840534fad334228ccf98a77454dea8a02bc
Vincent Nilam <vincent.nilam@hotmail.com>
2025-02-15 15:41:02 +1100

    feet(): add logo and put the login component inside a 50/50 container with a gradient bg

--- client/src/components/AuthPage.tsx
+++ client/src/components/AuthPage.tsx
@@ -5,12 +5,16 @@ import { useNavigate } from "react-router";
 
 const AuthPage: React.FC = () => {
 	const [authMode, setAuthMode] = useState<"login"|"signup">("login");
+	
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [email, setEmail] = useState('');
 	const [confirmPassword, setConfirmPassword] = useState('');
-	const [error, setError] = useState('');
 	const [showPassword, setShowPassword] = useState(false);
 	const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+	
+	const [error, setError] = useState('');
+	
 	const navigate = useNavigate();
 
 	const toggleAuthMode = () => setAuthMode(authMode === "login" ? "signup" : "login");
@@ -48,94 +52,102 @@ const AuthPage: React.FC = () => {
 			setError("An error occured. Please try again.");
 		}
 	};
-
 	return (
-		<div className="flex items-center justify-center h-screen bg-gray-100">
-			<div className="bg-white p-8 rounded-lg shadow-md w-96">
-				<h2 className="text-2xl font-bold mb-4 text-center">
-					{ authMode === "login" ? "Login" : "Sign Up"}
-				</h2>
-				
-				{error && <p className="text-red-500 mb-4">{error}</p>}
-
-				<form onSubmit={handleSubmit}>
-					<div className="mb-4">
-						<label htmlFor="username" className="block text-gray-700 font-bold mb-2">
-							Username
-						</label>
-						<input
-							type="text"
-							id="username"
-							className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"
-							value={username}
-							onChange={(e) => setUsername(e.target.value)}
-							placeholder="Enter your username"
-						/>
-					</div>
-					<div className="mb-6">
-						<label htmlFor="password" className="block text-gray-700 font-bold mb-2">
-							Password
-						</label>
-						<div className="relative">
-							<input
-								type={showPassword ? "text" : "password"}
-								id="password"
-								className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-								value={password}
-								onChange={(e) => setPassword(e.target.value)}
-								placeholder="Enter your password"
-								required
-							/>
-							<button
-								type="button"
-								className="absolute inset-y-0 right-0 px-3 py-2 focus:outline-none"
-								onClick={() => setShowPassword(!showPassword)}
-							>
-								{showPassword ? <FaEye className="text-gray-500" /> : <FaEyeSlash className="text-gray-500" />}
-							</button>
-						</div>
-					</div>
+		<div className="h-screen flex items-center justify-center bg-gradient-to-br from-[#072b8f] to-[rgb(218,223,231)]">
+			<div className="bg-white rounded-lg shadow-lg flex w-[1000px] h-[500px] overflow-hidden">		
+				{/* left */}
+				<div 
+						className="w-1/2 bg-cover bg-center flex items-center justify-center"
+						style={{ backgroundImage: "url('/src/assets/logo.jpg')" }}
+				>
+				</div>
+				{/* right */}
+				<div className="w-1/2 p-6 flex flex-col justify-center">
+					<h2 className="text-2xl font-bold mb-4 text-center">
+						{ authMode === "login" ? "Login" : "Sign Up"}
+					</h2>
 					
-					{authMode === "signup" && (
+					{error && <p className="text-red-500 mb-4">{error}</p>}
+
+					<form onSubmit={handleSubmit}>
+						<div className="mb-4">
+							<label htmlFor="username" className="block text-gray-700 font-bold mb-2">
+								Username
+							</label>
+							<input
+								type="text"
+								id="username"
+								className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 mb-2"
+								value={username}
+								onChange={(e) => setUsername(e.target.value)}
+								placeholder="Enter your username"
+							/>
+						</div>
 						<div className="mb-6">
-							<label htmlFor="confirmPassword" className="block text-gray-700 font-bold mb-2">
-								Confirm Password
+							<label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+								Password
 							</label>
 							<div className="relative">
 								<input
-									type={showConfirmPassword? "text" : "password"}
+									type={showPassword ? "text" : "password"}
+									id="password"
 									className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
-									value={confirmPassword}
-									onChange={(e) => setConfirmPassword(e.target.value)}
-									placeholder="Confirm your password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									placeholder="Enter your password"
 									required
 								/>
 								<button
 									type="button"
 									className="absolute inset-y-0 right-0 px-3 py-2 focus:outline-none"
-									onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+									onClick={() => setShowPassword(!showPassword)}
 								>
-									{showConfirmPassword ? <FaEye className="text-gray-500" /> : <FaEyeSlash className="text-gray-500" />}
+									{showPassword ? <FaEye className="text-gray-500" /> : <FaEyeSlash className="text-gray-500" />}
 								</button>
 							</div>
 						</div>
-					)}
-					<div className="flex justify-center">
-						<button
-							type="submit"
-							className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:opacity-50 cursor-pointer"
-						>
-							{authMode === "login" ? "Log In" : "Sign Up"}
-						</button>
+						
+						{authMode === "signup" && (
+							<div className="mb-6">
+								<label htmlFor="confirmPassword" className="block text-gray-700 font-bold mb-2">
+									Confirm Password
+								</label>
+								<div className="relative">
+									<input
+										type={showConfirmPassword? "text" : "password"}
+										className="border border-gray-300 rounded-md px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+										value={confirmPassword}
+										onChange={(e) => setConfirmPassword(e.target.value)}
+										placeholder="Confirm your password"
+										required
+									/>
+									<button
+										type="button"
+										className="absolute inset-y-0 right-0 px-3 py-2 focus:outline-none"
+										onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+									>
+										{showConfirmPassword ? <FaEye className="text-gray-500" /> : <FaEyeSlash className="text-gray-500" />}
+									</button>
+								</div>
+							</div>
+						)}
+						<div className="flex justify-center">
+							<button
+								type="submit"
+								className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300 disabled:opacity-50 cursor-pointer"
+							>
+								{authMode === "login" ? "Log In" : "Sign Up"}
+							</button>
+						</div>
+					</form>
+					<div className="mt-4 text-center">
+						<p className="text-gray-700">
+							{authMode === "login" ? "Don't have an account?" : "Already have an account?"}{" "}
+							<button className="text-blue-500 hover:underline cursor-pointer" onClick={toggleAuthMode}>
+								{authMode === "login" ? "Sign Up" : "Log In"}
+							</button>
+						</p>
 					</div>
-				</form>
-				<div className="mt-4 text-center">
-					<p className="text-gray-700">
-						{authMode === "login" ? "Don't have an account?" : "Already have an account?"}{" "}
-						<button className="text-blue-500 hover:underline cursor-pointer" onClick={toggleAuthMode}>
-							{authMode === "login" ? "Sign Up" : "Log In"}
-						</button>
-					</p>
 				</div>
 			</div>
 		</div>
